7196abb0744c9eae0ec76e6c6c6a7ae48beb44d9
Bitok85 <kan041185@gmail.com>
2023-01-13 16:26:57 +0300

    2. Монолитные системы [#338433 #380417]

--- dish/src/main/java/ru/job4j_fast_food/dish/service/DishService.java
+++ dish/src/main/java/ru/job4j_fast_food/dish/service/DishService.java
@@ -4,13 +4,13 @@ import ru.job4j_fast_food.domain.Dish;
 
 public interface DishService {
 
-    void addDish(String dishName);
+    void addDish(Dish dish);
 
     Dish removeDish(String dishName);
 
-    void addDishPrice(float price);
+    void addDishPrice(int id, float price);
 
-    float changeDishPrice(float price);
+    float changeDishPrice(int id, float price);
 
     boolean checkAvailability(String dishName);
 }
